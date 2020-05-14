ce5b5e05354e4ec859dd207019d28f94bd382d3e
Vendy Prum <vendy@thinkful.com>
2020-05-14 12:51:57 -0700

    second drill

--- drills.js
+++ drills.js
@@ -1,25 +1,16 @@
 //1. Understanding merge sort
+//a.3rd call is mergeSort[21]
+//b. 16th call is mergeSort[34]
+//c. first list: [1,21], second list [26,45]
+//d. merge[34,43]
+
+
 //21, 1, 26, 45, 29, 28, 2, 9, 16, 49, 39, 27, 43, 34, 46, 40
 
 //[21, 1, 26, 45, 29, 28, 2, 9]
 //[16, 49, 39, 27, 43, 34, 46, 40]
 
-//a.[21 1], [26,45],[29,28]
-//left:
-//[21, 1, 26, 45, 29, 28, 2, 9]
-//[21 1], [26,45], [29, 28], [2,9]
-//[1 21], [26,45], [28,29],[2,9]
-//1 21 26 45 28 29 2 9
-//right:[16, 49], [27, 39], [34, 43]
-// [16, 49], [39, 27], [43, 34], [46, 40]
-// [16, 49], [27, 39], [34, 43], [40, 46]
-// [16, 49, 27, 39, 34, 43, 40, 46]
-// [16, 27, 34, 39, 40, 43, 46]
 
-//a.3rd call is mergeSort[21]
-//b. 16th call is mergeSort[34]
-//c. first list: [1,21], second list [26,45]
-//d. merge[34,43]
 
 // 21, 1, 26, 45, 29, 28, 2, 9, 16, 49, 39, 27, 43, 34, 46, 40
 // [1, 2, 9, 16, ... 45] [46, 49]
@@ -31,7 +22,7 @@
 //right mergeSort([16, 49], [39, 27], [43,34], [46,40])
 
 //3rd call to left mergeSort([21][1],[26][45],[29][28],[2][9])
-  //mergeSort[21] mergeSort[1], mergeSort[26] mergeSort[45], mergeSort[29] mergeSort[28], mergeSort[2] mergeSort[9]
+//mergeSort[21] mergeSort[1], mergeSort[26] mergeSort[45], mergeSort[29] mergeSort[28], mergeSort[2] mergeSort[9]
 //right mergeSort([16][49],[39][27],[43][34],[46][40])
 //mergeSort[16] mergeSort[49], mergeSort[39] mergeSort[27], mergeSort[43] mergeSort[34], mergeSort[46] mergeSort[40]
 //left: merge[1,21] merge[26,45], merge[28,29], merge[2,9]
@@ -43,51 +34,155 @@
 //merge [1,2,9,16,21,26,27,28,29,34,39,40,43,45,46]
 
 
-const bubbleSort = arr => {
-  // count how many iterations 
-  let sorts = 0;
-  //loop through, - 1 from length for each step in the sort
-  for(let i = 0; i < arr.length - 1; i++) {
-    
-  }
-}
 
-
-
+//2.Understanding Quicksort
+//a. Pivot could be either 14, or 17
+//values below 14 or 17 are less, 
+//values above 14 or 17 are higher
+//b.  9, answer[16], 13, 15, 19, 10, 3, 17, 14, pivot(12)
+// pivot(14), 17, 12, 15, answer[3], 10, 19, 16, 9, 13
 //breaks array down into continually smaller chunks
 //then merges them back together
-function mergeSort(array) {
-  if (array.length <= 1) {
-    return array;
-  }
 
-  const middle = Math.floor(array.length / 2);
-  let left = array.slice(0, middle);
-  let right = array.slice(middle, array.length);
 
-  left = mergeSort(left);
-  right = mergeSort(right);
-  return merge(left, right, array);
+function swap(array, i, j) {
+  const tmp = array[i];
+  array[i] = array[j];
+  array[j] = tmp;
 }
 
-function merge(left, right, array) {
-  let leftIndex = 0;
-  let rightIndex = 0;
-  let outputIndex = 0;
-  while (leftIndex < left.length && rightIndex < right.length) {
-    if (left[leftIndex] < right[rightIndex]) {
-      array[outputIndex++] = left[leftIndex++];
-    } else {
-      array[outputIndex++] = right[rightIndex++];
+const partition = (arr, start, end) => {
+  // final value in array too look for
+  const pivot = arr[end - 1];
+  let j = start;
+  for (let i = 0; i < end - 1; i++) {
+    console.log(arr);
+    if (arr[i] <= pivot) {
+      console.log(arr);
+      swap(arr, i, j);
+      console.log(arr);
+      j++;
     }
   }
+  console.log(arr);
+  swap(arr, end - 1, j);
+  console.log(arr);
+  return j;
+};
 
-  for (let i = leftIndex; i < left.length; i++) {
-    array[outputIndex++] = left[i];
+const quickSort = (arr, start = 0, end = arr.length) => {
+  if (start >= end) {
+    return arr;
   }
+  const middle = partition(arr, start, end);
+  console.log(arr);
+  arr = quickSort(arr, start, middle);
+  console.log(arr);
+  arr = quickSort(arr, middle + 1, end);
+  return arr;
+};
 
-  for (let i = rightIndex; i < right.length; i++) {
-    array[outputIndex++] = right[i];
-  }
-  return array;
-}
+console.log(quickSort(qSortArray));
+
+const qSortArray = [
+  89,
+  30,
+  25,
+  32,
+  72,
+  70,
+  51,
+  42,
+  25,
+  24,
+  53,
+  55,
+  78,
+  50,
+  13,
+  40,
+  48,
+  32,
+  26,
+  2,
+  14,
+  33,
+  45,
+  72,
+  56,
+  44,
+  21,
+  88,
+  27,
+  68,
+  15,
+  62,
+  93,
+  98,
+  73,
+  28,
+  16,
+  46,
+  87,
+  28,
+  65,
+  38,
+  67,
+  16,
+  85,
+  63,
+  23,
+  69,
+  64,
+  91,
+  9,
+  70,
+  81,
+  27,
+  97,
+  82,
+  6,
+  88,
+  3,
+  7,
+  46,
+  13,
+  11,
+  64,
+  76,
+  31,
+  26,
+  38,
+  28,
+  13,
+  17,
+  69,
+  90,
+  1,
+  6,
+  7,
+  64,
+  43,
+  9,
+  73,
+  80,
+  98,
+  46,
+  27,
+  22,
+  87,
+  49,
+  83,
+  6,
+  39,
+  42,
+  51,
+  54,
+  84,
+  34,
+  53,
+  78,
+  40,
+  14,
+  5,
+];
